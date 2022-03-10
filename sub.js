15b447cf3f20d6568af68297e78fb6c0f7de6e3a
NUMBRITHBOPANNA <numbrithbopanna25@gmail.com>
2022-03-10 16:33:43 +0530

    done with js

--- sub.js
+++ sub.js
@@ -1,5 +1,7 @@
-var a
-var b 
+var a = document.getElementById("num1").value
+var b = document.getElementById("num2").value
 var result
 result =a-b
+
+document.getElementById("result").value = result
 
