4c26215f865b1cc59b73a357c67bbc4e6bf199c0
Darshhgowda <Darshangowda@protonmail.com>
2022-03-10 16:46:20 +0530

    Updated

--- Add.js
+++ Add.js
@@ -1,7 +1,7 @@
 var a=document.getElemetById("num1").value
 var b=document.getElemetById("num2").value
 var res
-res=a+b
-document.getElemetById("res").value
+result=a+b
+document.getElemetById("result").value
 
 
