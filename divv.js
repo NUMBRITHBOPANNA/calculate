33632ef120f816d39b04fa9aa14e4d20daea1b06
Pavithra-MB1 <pavithrabmadhusudan@gmail.com>
2022-03-10 16:58:46 +0530

    division function code

--- divv.js
+++ divv.js
@@ -1,3 +1,7 @@
-var a
-var b
-var result=a\b
+function()
+{
+var a = document.getElementById("num1").value
+var b = document.getElementById("num2").value
+var result = document.getElementById("result").value
+result = a \ b
+}
